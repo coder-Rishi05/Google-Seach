e5ca79d9f9a03149aafaf183462c6911faa6c5fd
Rishabh Rawat <rawatrishi1121@gmail.com>
2025-02-18 22:34:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const cls = document.querySelector("#icon");
+const btn = document.querySelector("button");
+const main = document.querySelector("main");
+const popUp = document.querySelector(".dialog-box");
+
+cls.addEventListener("click", () => {
+  //    console.log("hi")
+  popUp.style.display = "none";
+});
